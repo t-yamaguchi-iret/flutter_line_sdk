2a317fe1289e9bca5ae0cc51a612a5b2cc1c9d87
Tatsuya Yamaguchi <t-yamaguchi@cloudpack.jp>
2022-02-17 10:44:09 +0900

    xxx-Swift.hが見つからない問題のW/A
    詳細はこのIssueを参照
    flutter/flutter#25676

--- ios/Classes/FlutterLineSdkPlugin.m
+++ ios/Classes/FlutterLineSdkPlugin.m
@@ -1,5 +1,10 @@
 #import "FlutterLineSdkPlugin.h"
+
+#if __has_include(<flutter_line_sdk/flutter_line_sdk-Swift.h>)
 #import <flutter_line_sdk/flutter_line_sdk-Swift.h>
+#else
+#import "flutter_line_sdk-Swift.h"
+#endif
 
 @implementation FlutterLineSdkPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
